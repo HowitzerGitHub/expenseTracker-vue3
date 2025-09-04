2ad4303fbcb669f35a650e2eabf2147c15c213f1
CharchitRzing <charchit.rajput@zingtree.com>
2025-09-05 00:48:55 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance></Balance>
+    <IncomeExpenses></IncomeExpenses>
+    <TransactionList></TransactionList>
+    <AddTransaction></AddTransaction>
+  </div>
+</template>
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import addTransaction from "./components/addTransaction.vue";
+import AddTransaction from "./components/addTransaction.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,2 @@
+<template>Expense Tracker</template>
+<script></script>
